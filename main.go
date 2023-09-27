b2652450c840f7adb1431a500ae4da419f9df43b
Aslanchik2o <admroblols@gmail.com>
2023-09-27 21:09:48 +0500

    Start

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Cache struct {
+    items map[string]Item
+}
+
+type Item struct {
+    Value interface{}
+    
+}
+
+func New() *Cache {
+
+    items := make(map[string]Item)
+
+    cache := Cache{
+        items: items,
+    }
+    return &cache
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+    c.items[key] = Item{
+        Value:      value,
+    }
+
+}
+
+func (c *Cache) Delete(key string)  interface{} {
+     item := c.items[key]  
+    delete(c.items, key)
+fmt.Println(item)
+return item
+}
