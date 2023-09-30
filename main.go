6c33f89fe4ce497d7deaf2106c3724da360c5059
Aslanchik2o <admroblols@gmail.com>
2023-09-30 15:46:00 +0500

    add set func

--- main.go
+++ main.go
@@ -5,34 +5,33 @@ import (
 )
 
 type Cache struct {
-    items map[string]Item
+	items map[string]Item
 }
 
 type Item struct {
-    Value interface{}
-    
+	Value interface{}
 }
 
 func New() *Cache {
 
-    items := make(map[string]Item)
+	items := make(map[string]Item)
 
-    cache := Cache{
-        items: items,
-    }
-    return &cache
+	cache := Cache{
+		items: items,
+	}
+	return &cache
 }
 
 func (c *Cache) Set(key string, value interface{}) {
-    c.items[key] = Item{
-        Value:      value,
-    }
+	c.items[key] = Item{
+		Value: value,
+	}
 
 }
 
-func (c *Cache) Delete(key string)  interface{} {
-     item := c.items[key]  
-    delete(c.items, key)
-fmt.Println(item)
-return item
+func (c *Cache) Delete(key string) interface{} {
+	item := c.items[key]
+	delete(c.items, key)
+	fmt.Println(item)
+	return item
 }
