4a6ea4054653d0dd0cad00d573ce448f872936fa
Aslanchik2o <admroblols@gmail.com>
2023-10-02 13:25:16 +0500

    bla

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Cache struct {
-	items map[string]Item
+	items map[int]Item
 }
 
 type Item struct {
@@ -14,7 +14,7 @@ type Item struct {
 
 func New() *Cache {
 
-	items := make(map[string]Item)
+	items := make(map[int]Item)
 
 	cache := Cache{
 		items: items,
@@ -22,18 +22,18 @@ func New() *Cache {
 	return &cache
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) Set(key int, value interface{}) {
 	c.items[key] = Item{
 		Value: value,
 	}
 }
 
-func (c *Cache) Get(key string) interface{} {
+func (c *Cache) Get(key int) interface{} {
 	item := c.items[key]
 	return item.Value
 }
 
-func (c *Cache) Delete(key string) interface{} {
+func (c *Cache) Delete(key int) interface{} {
 	item := c.items[key]
 	delete(c.items, key)
 	fmt.Println(item)
